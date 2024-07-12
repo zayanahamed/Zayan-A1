b0c38dd8da33f5af10c3efe9198537b01df6978b
zayanahamed <zayanahamed>
2024-07-13 02:42:33 +0500

    Working on the ChildSpecialist Class

--- src/ChildSpecialist.java
+++ src/ChildSpecialist.java
@@ -5,4 +5,16 @@ public class ChildSpecialist {
     public ChildSpecialist() {
 
     }
+
+    public ChildSpecialist(int id, String name, String basicInformation, String expertise) {
+        super(id, name, basicInformation);
+        this.expertise = expertise;
+    }
+
+    public void printChildSpecialistDetails() {
+        System.out.println("The doctor details are:");
+        super.printAllDetailsOfHealthProfessional();
+        System.out.println("HealthProfessional's Expertise: " + expertise);
+        System.out.println("HealthProfessional Type: ChildSpecialist");
+    }
 }
